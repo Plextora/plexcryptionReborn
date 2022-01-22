2cc424d8549bad36d23a026c0948d9ae54d92fd1
Plextora <plextoraa.a@gmail.com>
2022-01-21 20:51:00 -0500

    Add functions to encrypt and decrypt text

--- plexcryptionReborn.py
+++ plexcryptionReborn.py
@@ -1,6 +1,94 @@
 import time
 import os
 
+def encryptText():
+    print("Please enter the text you want to encrypt:")
+
+    text = input(">> ")
+    text = text.upper()
+
+    encryption = {
+        "A": "¯\_(ツ)_/¯",
+        "B": "( ͡° ͜ʖ ͡°)",
+        "C": "ʕ•ᴥ•ʔ",
+        "D": "▄︻̷̿┻̿═━一",
+        "E": "༼ つ ◕_◕ ༽つ",
+        "F": "ಠ_ಠ",
+        "G": "(◕‿◕✿)",
+        "H": "(¬‿¬)",
+        "I": "◉_◉",
+        "J": "~(˘▾˘~)",
+        "K": "ヾ(⌐■_■)ノ♪",
+        "L": "ಥ_ಥ",
+        "M": "(｡◕‿◕｡)",
+        "N": "°Д°",
+        "O": "(；一_一)",
+        "P": "( ⚆ _ ⚆ )",
+        "Q": "¯\(°_o)/¯",
+        "R": "｡◕‿◕｡",
+        "S": "ಠ~ಠ",
+        "T": "(>ლ)",
+        "U": "Ƹ̵̡Ӝ̵̨̄Ʒ",
+        "V": "(▰˘◡˘▰)",
+        "W": "◔̯◔",
+        "X": "¬_¬",
+        "Y": "._.",
+        "Z": ":D"
+    }
+
+    for key, value in encryption.items():
+        text = text.replace(key, value)
+    
+    print("Here is your encrypted text:")    
+        
+    print(text)
+
+    menu()
+
+def decryptText():
+    print("Please enter the text you want to decrypt:")
+
+    text = input(">> ")
+    text = text.upper()
+
+    decryption = {
+        "¯\_(ツ)_/¯": "A",
+        "( ͡° ͜ʖ ͡°)": "B",
+        "ʕ•ᴥ•ʔ": "C",
+        "▄︻̷̿┻̿═━一": "D",
+        "༼ つ ◕_◕ ༽つ": "E",
+        "ಠ_ಠ": "F",
+        "(◕‿◕✿)": "G",
+        "(¬‿¬)": "H",
+        "◉_◉": "I",
+        "~(˘▾˘~)": "J",
+        "ヾ(⌐■_■)ノ♪": "K",
+        "ಥ_ಥ": "L",
+        "(｡◕‿◕｡)": "M",
+        "°Д°": "N",
+        "(；一_一)": "O",
+        "( ⚆ _ ⚆ )": "P",
+        "¯\(°_o)/¯": "Q",
+        "｡◕‿◕｡": "R",
+        "ಠ~ಠ": "S",
+        "(>ლ)": "T",
+        "Ƹ̵̡Ӝ̵̨̄Ʒ": "U",
+        "(▰˘◡˘▰)": "V",
+        "◔̯◔": "W",
+        "¬_¬": "X",
+        "._.": "Y",
+        ":D": "Z"
+    }
+
+    for key, value in decryption.items():
+        text = text.replace(key, value)
+    
+    print("Here is your decrypted text:")
+
+    print(text)
+
+    menu()
+
 
 def startup():
     os.system('cls' if os.name == 'nt' else 'clear')
